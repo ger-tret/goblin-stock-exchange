2727568423d61058061821a008fb7845c22ba5c2
German Tretyakevich <ger.tretyakevich@gmail.com>
2026-06-30 16:33:32 +0300

    feat: define core domain model, enums and DTO layer

---
+++ src/main/java/me/ger_tret/goblin_stock_exchange/entity/Asset.java
@@ -0,0 +1,36 @@
+package me.ger_tret.goblin_stock_exchange.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import me.ger_tret.goblin_stock_exchange.entity.constant.AssetType;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "assets")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Asset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true, length = 10)
+    private String ticker;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "asset_type")
+    private AssetType type;
+
+
+    @Column(name = "base_price", nullable = false)
+    private BigDecimal basePrice;
+
+    private String description;
+}
---
+++ src/main/java/me/ger_tret/goblin_stock_exchange/entity/constant/AssetType.java
@@ -0,0 +1,5 @@
+package me.ger_tret.goblin_stock_exchange.entity.constant;
+
+public enum AssetType {
+    SLIME, RUNE, ARTIFACT, WEAPON, INGREDIENT
+}
